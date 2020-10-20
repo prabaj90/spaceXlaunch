bf14f1ba7fcaa67f2c3f8e66caf0c9773e36b913
prabaj90 <prabaj90@gmail.com>
2020-10-20 18:53:10 +0530

    Land Success done

---
+++ space-launch/src/components/constant.js
@@ -0,0 +1,2 @@
+//URL = "https://api.spacexdata.com/v3/launches?limit=10";
+export const URL = "https://api.spaceXdata.com/v3/launches?limit=100";
--- space-launch/src/components/space.js
+++ space-launch/src/components/space.js
@@ -1,8 +1,7 @@
 import React, {Component} from 'react';
 import axios from 'axios';
+import {URL} from './constant';
 // import './style.scss';
-//URL = "https://api.spacexdata.com/v3/launches?limit=10"; // URL variable stores JSON url || API taken from 10 Degrees WordPress Agency
-const URL = "https://api.spaceXdata.com/v3/launches?limit=100&amp;launch_success=true";
 
 export default class AppDev extends Component {
 
@@ -50,10 +49,11 @@ _filterByLaunch = launch => {
 	item.launch_success === launch
 	);
 	this.setState({ post: post });
-	}
+    }
+    
   _filterByLanding = landing => {
 	const post = this.state.allPosts.filter(item =>
-	item.launch_landing === landing
+	item.rocket.first_stage.cores[0].land_success === landing
 	);
 	this.setState({ post:post });
 	}
@@ -65,7 +65,7 @@ _filterByLaunch = launch => {
       
 			<div className="container-fluid">
         <header>
-  <h2>project</h2>
+  <h2>Project</h2>
 </header>
         <section>
         <nav>
@@ -75,58 +75,58 @@ _filterByLaunch = launch => {
     <ul>
       <h1>Filters</h1>
     <p>Launch Year</p>
-        <div class="w3-show-inline-block">
-  <div class="w3-bar">
-    <button class="w3-btn w3-teal"  onClick={() => this._filterByYear('2006')}>2006</button>
-    <button class="w3-btn w3-teal"  onClick={() => this._filterByYear('2007')}>2007</button>
+        <div className="">
+  <div className="">
+    <button className=""  onClick={() => this._filterByYear('2006')}>2006</button>
+    <button className=""  onClick={() => this._filterByYear('2007')}>2007</button>
     
   </div>
   </div>
-  <div class="w3-show-inline-block">
-  <div class="w3-bar">
-    <button class="w3-btn w3-teal"  onClick={() => this._filterByYear('2008')}>2008</button>
-    <button class="w3-btn w3-teal"  onClick={() => this._filterByYear('2009')}>2009</button>
+  <div className="">
+  <div className="">
+    <button className=""  onClick={() => this._filterByYear('2008')}>2008</button>
+    <button className=""  onClick={() => this._filterByYear('2009')}>2009</button>
     
   </div>
   </div>
-  <div class="w3-show-inline-block">
-  <div class="w3-bar">
-    <button class="w3-btn w3-teal"  onClick={() => this._filterByYear('2010')}>2010</button>
-    <button class="w3-btn w3-teal"  onClick={() => this._filterByYear('2011')}>2011</button>
+  <div className="">
+  <div className="">
+    <button className=""  onClick={() => this._filterByYear('2010')}>2010</button>
+    <button className=""  onClick={() => this._filterByYear('2011')}>2011</button>
     
   </div>
   </div>
-  <div class="w3-show-inline-block">
-  <div class="w3-bar">
-    <button class="w3-btn w3-teal"  onClick={() => this._filterByYear('2012')}>2012</button>
-    <button class="w3-btn w3-teal"  onClick={() => this._filterByYear('2013')}>2013</button>
+  <div className="">
+  <div className="">
+    <button className=""  onClick={() => this._filterByYear('2012')}>2012</button>
+    <button className=""  onClick={() => this._filterByYear('2013')}>2013</button>
     
   </div>
   </div>
-  <div class="w3-show-inline-block">
-  <div class="w3-bar">
-    <button class="w3-btn w3-teal"  onClick={() => this._filterByYear('2014')}>2014</button>
-    <button class="w3-btn w3-teal"  onClick={() => this._filterByYear('2015')}>2015</button>
+  <div className="">
+  <div className="">
+    <button className=""  onClick={() => this._filterByYear('2014')}>2014</button>
+    <button className=""  onClick={() => this._filterByYear('2015')}>2015</button>
     
   </div>
   </div>
-  <div class="w3-show-inline-block">
-  <div class="w3-bar">
-    <button class="w3-btn w3-teal"  onClick={() => this._filterByYear('2016')}>2016</button>
-    <button class="w3-btn w3-teal"  onClick={() => this._filterByYear('2017')}>2017</button>
+  <div className="">
+  <div className="">
+    <button className=""  onClick={() => this._filterByYear('2016')}>2016</button>
+    <button className=""  onClick={() => this._filterByYear('2017')}>2017</button>
     
   </div>
   </div>
-  <div class="w3-show-inline-block">
-  <div class="w3-bar">
-    <button class="w3-btn w3-teal"  onClick={() => this._filterByYear('2018')}>2018</button>
-    <button class="w3-btn w3-teal"  onClick={() => this._filterByYear('2019')}>2019</button>
+  <div className="">
+  <div className="">
+    <button className=""  onClick={() => this._filterByYear('2018')}>2018</button>
+    <button className=""  onClick={() => this._filterByYear('2019')}>2019</button>
     
   </div>
   </div>
-  <div class="w3-show-inline-block">
-  <div class="w3-bar">
-    <button class="w3-btn w3-teal"  onClick={() => this._filterByYear('2020')}>2020</button>
+  <div className="">
+  <div className="">
+    <button class=""  onClick={() => this._filterByYear('2020')}>2020</button>
     
     
   </div>
@@ -134,20 +134,20 @@ _filterByLaunch = launch => {
       <br/>
   <p>Successful Launch</p>
     
-	  <div class="w3-show-inline-block">
-  <div class="w3-bar">
-    <button class="btn btn-success"  onClick={() => this._filterByLaunch(true)}>true</button>
-  <button class="btn btn-success"  onClick={() => this._filterByLaunch(false)}>false</button>
+	  <div className="">
+  <div className="">
+    <button className=""  onClick={() => this._filterByLaunch(true)}>true</button>
+  <button className=""  onClick={() => this._filterByLaunch(false)}>false</button>
     
   </div>
   </div>
   <br/>
   <p>Successful Landing</p>
     
-	  <div class="w3-show-inline-block">
-  <div class="w3-bar">
-     <button class="btn btn-success"  onClick={() => this._filterByLanding(true)}>true</button>
-  <button class="btn btn-success"  onClick={() => this._filterByLanding(false)}>false</button>
+	  <div className="">
+  <div className="">
+     <button className=""  onClick={() => this._filterByLanding(true)}>true</button>
+  <button className=""  onClick={() => this._filterByLanding(false)}>false</button>
   </div>
   </div>
     </ul>
@@ -165,7 +165,7 @@ _filterByLaunch = launch => {
 <h5>Mission Ids: {item.mission_id.length === 0 ? "0" : item.mission_id.map((id, index) => <span key={index}>{ (index ? ', ' : '') + id }</span>)}</h5>
 <h5>Launch Year: {item.launch_year}</h5>
 <h5>Successful Launch: {item.launch_success ? "True" : "False"}</h5>
-<h5>Successful Landing: {item.launch_landing ? "True" : "False"}</h5>
+<h5>Successful Landing: {item.rocket.first_stage.cores[0].land_success ? "True" : "False"}</h5>
 </div>
 </li>
 ))}
