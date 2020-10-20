a301702f3fdf9bd52d711216829494858ce8b5c1
prabaj90 <prabaj90@gmail.com>
2020-10-21 00:33:15 +0530

    Fucntion updated

--- space-launch/src/components/space.js
+++ space-launch/src/components/space.js
@@ -1,174 +1,177 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
-import {URL} from './constant';
+import { URL } from './constant';
 
 export default class AppDev extends Component {
 
-    constructor(props){
-        super(props);
-        this.state = {
-            post: [],
-            allPosts: []
-        };    
+  constructor(props) {
+    super(props);
+    this.state = {
+      post: [],
+      allPosts: []
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get(URL, {
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json"
+        }
+      })
+      .then(({ data }) => {
+        this.setState({
+          post: data,
+          allPosts: data // array data from JSON stored in these
+        });
+      })
+      .catch(err => { });
+  }
+
+  filterByYear = (key, e) => {
+    const { allPosts } = this.state;
+    if (key === 'year') {
+      const post = allPosts.filter(item =>
+        item.launch_year === e.target.innerHTML
+      );
+      this.setState({ post: post });
+    }
+    else if (key === 'launch') {
+      const post = allPosts.filter(item =>
+        item.launch_success === JSON.parse((e.target.innerHTML).toLowerCase())
+      );
+      this.setState({ post: post });
+    }
+    else if (key === 'landing') {
+      const post = allPosts.filter(item =>
+        item.rocket.first_stage.cores[0].land_success === JSON.parse((e.target.innerHTML).toLowerCase())
+      );
+      this.setState({ post: post });
+    }
+    else {
+      this.setState({ post: allPosts });
     }
 
-	componentDidMount() {
-		axios
-			.get(URL, {
-				headers: {
-					Accept: "application/json",
-					"Content-Type": "application/json"
-				}
-			})
-			.then(({ data }) => {
-				this.setState({
-          post: data,
-					allPosts: data // array data from JSON stored in these
-				});
-			})
-			.catch(err => {});
-	}
-  
-  filterByYear = year => {
-        const post = this.state.allPosts.filter(item =>
-            item.launch_year === year
-        );
-        this.setState({ post: post });
-}
-filterByLaunch = launch => {
-	const post = this.state.allPosts.filter(item =>
-	item.launch_success === launch
-	);
-	this.setState({ post: post });
-    }
-    
-  filterByLanding = landing => {
-	const post = this.state.allPosts.filter(item =>
-	item.rocket.first_stage.cores[0].land_success === landing
-	);
-	this.setState({ post:post });
-	}
-  
-  
-  
-	render() {
-		return (
-      
-			<div className="container-fluid">
+  }
+  render() {
+    return (
+
+      <div className="container-fluid">
         <header>
-  <h2>SpaceX Launch Programs</h2>
-</header>
+          <h2>SpaceX Launch Programs</h2>
+        </header>
 
         <section>
-        <nav>
- 
-          
-          
-    <ul>
-      <h1>Filters</h1>
-    <p>Launch Year</p>
-        <div className="">
-  <div className="">
-    <button className=""  onClick={() => this.filterByYear('2006')}>2006</button>
-    <button className=""  onClick={() => this.filterByYear('2007')}>2007</button>
-    
-  </div>
-  </div>
-  <div className="">
-  <div className="">
-    <button className=""  onClick={() => this.filterByYear('2008')}>2008</button>
-    <button className=""  onClick={() => this.filterByYear('2009')}>2009</button>
-    
-  </div>
-  </div>
-  <div className="">
-  <div className="">
-    <button className=""  onClick={() => this.filterByYear('2010')}>2010</button>
-    <button className=""  onClick={() => this.filterByYear('2011')}>2011</button>
-    
-  </div>
-  </div>
-  <div className="">
-  <div className="">
-    <button className=""  onClick={() => this.filterByYear('2012')}>2012</button>
-    <button className=""  onClick={() => this.filterByYear('2013')}>2013</button>
-    
-  </div>
-  </div>
-  <div className="">
-  <div className="">
-    <button className=""  onClick={() => this.filterByYear('2014')}>2014</button>
-    <button className=""  onClick={() => this.filterByYear('2015')}>2015</button>
-    
-  </div>
-  </div>
-  <div className="">
-  <div className="">
-    <button className=""  onClick={() => this.filterByYear('2016')}>2016</button>
-    <button className=""  onClick={() => this.filterByYear('2017')}>2017</button>
-    
-  </div>
-  </div>
-  <div className="">
-  <div className="">
-    <button className=""  onClick={() => this.filterByYear('2018')}>2018</button>
-    <button className=""  onClick={() => this.filterByYear('2019')}>2019</button>
-    
-  </div>
-  </div>
-  <div className="">
-  <div className="">
-    <button class=""  onClick={() => this.filterByYear('2020')}>2020</button>
-    
-    
-  </div>
-  </div>
-      <br/>
-  <p>Successful Launch</p>
-    
-	  <div className="">
-  <div className="">
-    <button className=""  onClick={() => this.filterByLaunch(true)}>True</button>
-  <button className=""  onClick={() => this.filterByLaunch(false)}>False</button>
-    
-  </div>
-  </div>
-  <br/>
-  <p>Successful Landing</p>
-    
-	  <div className="">
-  <div className="">
-     <button className=""  onClick={() => this.filterByLanding(true)}>True</button>
-  <button className=""  onClick={() => this.filterByLanding(false)}>False</button>
-  </div>
-  </div>
-    </ul>
-  </nav>
-          
-            
+          <nav>
+
+
+
+            <ul>
+              <h1>Filters</h1>
+              <p>Launch Year</p>
+              <div className="">
+                <div className="">
+                  <button className="" onClick={(e) => this.filterByYear('year', e)}>2006</button>
+                  <button className="" onClick={(e) => this.filterByYear('year', e)}>2007</button>
+
+                </div>
+              </div>
+              <div className="">
+                <div className="">
+                  <button className="" onClick={(e) => this.filterByYear('year', e)}>2008</button>
+                  <button className="" onClick={(e) => this.filterByYear('year', e)}>2009</button>
+
+                </div>
+              </div>
+              <div className="">
+                <div className="">
+                  <button className="" onClick={(e) => this.filterByYear('year', e)}>2010</button>
+                  <button className="" onClick={(e) => this.filterByYear('year', e)}>2011</button>
+
+                </div>
+              </div>
+              <div className="">
+                <div className="">
+                  <button className="" onClick={(e) => this.filterByYear('year', e)}>2012</button>
+                  <button className="" onClick={(e) => this.filterByYear('year', e)}>2013</button>
+
+                </div>
+              </div>
+              <div className="">
+                <div className="">
+                  <button className="" onClick={(e) => this.filterByYear('year', e)}>2014</button>
+                  <button className="" onClick={(e) => this.filterByYear('year', e)}>2015</button>
+
+                </div>
+              </div>
+              <div className="">
+                <div className="">
+                  <button className="" onClick={(e) => this.filterByYear('year', e)}>2016</button>
+                  <button className="" onClick={(e) => this.filterByYear('year', e)}>2017</button>
+
+                </div>
+              </div>
+              <div className="">
+                <div className="">
+                  <button className="" onClick={(e) => this.filterByYear('year', e)}>2018</button>
+                  <button className="" onClick={(e) => this.filterByYear('year', e)}>2019</button>
+
+                </div>
+              </div>
+              <div className="">
+                <div className="">
+                  <button class="" onClick={() => this.filterByYear('2020')}>2020</button>
+
+
+                </div>
+              </div>
+              <br />
+              <p>Successful Launch</p>
+
+              <div className="">
+                <div className="">
+                  <button className="" onClick={(e) => this.filterByYear('launch', e)}>True</button>
+                  <button className="" onClick={(e) => this.filterByYear('launch', e)}>False</button>
+
+                </div>
+              </div>
+              <br />
+              <p>Successful Landing</p>
+
+              <div className="">
+                <div className="">
+                  <button className="" onClick={(e) => this.filterByYear('landing', e)}>True</button>
+                  <button className="" onClick={(e) => this.filterByYear('landing', e)}>False</button>
+                </div>
+              </div>
+            </ul>
+          </nav>
+
+
           <article>
-   <ul className="data-list">
-{/* post items mapped in a list linked to onKeyUp function */}
-{this.state.post.map((item, index) => (
-<li className={"block-" + index}>
-<div className="title">
-<img src={item.links.mission_patch_small} />
-<h3>{item.mission_name}  #{item.flight_number}</h3>
-<h5>Mission Ids: {item.mission_id.length === 0 ? "0" : item.mission_id.map((id, index) => <span key={index}>{ (index ? ', ' : '') + id }</span>)}</h5>
-<h5>Launch Year: {item.launch_year}</h5>
-<h5>Successful Launch: {item.launch_success ? "True" : "False"}</h5>
-<h5>Successful Landing: {item.rocket.first_stage.cores[0].land_success ? "True" : "False"}</h5>
-</div>
-</li>
-))}
-</ul>
-  </article>
-            
+            <ul className="data-list">
+              {/* post items mapped in a list linked to onKeyUp function */}
+              {this.state.post.map((item, index) => (
+                <li className={"block-" + index}>
+                  <div className="title">
+                    <img src={item.links.mission_patch_small} />
+                    <h3>{item.mission_name}  #{item.flight_number}</h3>
+                    <h5>Mission Ids: {item.mission_id.length === 0 ? "0" : item.mission_id.map((id, index) => <span key={index}>{(index ? ', ' : '') + id}</span>)}</h5>
+                    <h5>Launch Year: {item.launch_year}</h5>
+                    <h5>Successful Launch: {item.launch_success ? "True" : "False"}</h5>
+                    <h5>Successful Landing: {item.rocket.first_stage.cores[0].land_success ? "True" : "False"}</h5>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </article>
+
         </section>
-          
-        
-				
-			</div>
-		);
-	}
+
+
+
+      </div>
+    );
+  }
 }
